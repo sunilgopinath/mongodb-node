6889df8b13321630eea9f53928914626339e83a0
sunilgopinath <sxg282@gmail.com>
2014-03-26 16:46:32 -0400

    setting up page not found route

--- hello_express/app.js
+++ hello_express/app.js
@@ -3,6 +3,8 @@ var express = require('express'), app = express();
 app.get('/', function(req, res){
 	res.send("Hello, World!");
 });
-
+app.get('*', function(req, res){
+	res.send("Page not found", 404);
+});
 app.listen(8009);
 console.log("server was started at http://localhost:8009");
