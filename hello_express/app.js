9442cab7c2726069048e3f2b85bd4ec38ceeb53d
sunilgopinath <sxg282@gmail.com>
2014-03-26 17:47:34 -0400

    setting up hello swig

--- hello_express/app.js
+++ hello_express/app.js
@@ -1,16 +1,26 @@
 var express = require('express'), 
               app = express(),
-              cons = require('consolidate');
+              cons = require('consolidate')
+              MongoClient = require('mongodb').MongoClient,
+              Server = require('mongodb').Server;
 
 app.engine('html', cons.swig);
 app.set('view engine', 'html');
 app.set('views', __dirname + "/views");
 
+var mongoclient = new MongoClient(new Server('localhost', 27017,{ 'native_parser' : true }));
+var db = mongoclient.db('course');
+
 app.get('/', function(req, res){
-	res.render('hello', { 'name' : 'Swig' });
+	db.collection('hello_mongo_express').findOne({}, function(err, doc) {
+		res.render('hello',doc);
+		});
 });
 app.get('*', function(req, res){
 	res.send("Page not found", 404);
 });
-app.listen(8009);
-console.log("server was started at http://localhost:8009");
+mongoclient.open(function(err, mongoclient) {
+        if(err) throw err;
+	app.listen(8009);
+	console.log("Express server started at http://localhost:8009");
+});
